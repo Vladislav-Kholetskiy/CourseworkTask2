7e612431a82f40cca6ba32e029d0369fec424fdc
Vladislav-Kholetskiy <vladislav.kholetsky@gmail.com>
2025-06-13 14:46:32 +0300

    update1

--- src/main/java/Coursework/Main.java
+++ src/main/java/Coursework/Main.java
@@ -1,17 +1,65 @@
 package Coursework;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import Coursework.core.Observable;
+import Coursework.core.Observer;
+import Coursework.schedulers.ComputationScheduler;
+import Coursework.schedulers.IOThreadScheduler;
+import Coursework.schedulers.SingleThreadScheduler;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+import java.util.concurrent.CountDownLatch;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        CountDownLatch doneLatch = new CountDownLatch(1);
+
+        Observable.<Integer>create(emitter -> {
+                    for (int i = 1; i <= 5; i++) {
+                        emitter.onNext(i);
+                    }
+                    emitter.onComplete();
+                })
+                // 1) Эмиссия на пуле для I/O
+                .subscribeOn(new IOThreadScheduler())
+                // 2) Фильтрация чётных чисел
+                .filter(i -> i % 2 == 0)
+                // 3) Преобразование в строку
+                .map(i -> "Value: " + i)
+                // 4) Для каждого создаём под-Observable с задержкой
+                .flatMap(str ->
+                        Observable.<String>create(em -> {
+                                    try {
+                                        Thread.sleep(100); // имитируем работу
+                                        em.onNext(str + " (processed)");
+                                        em.onComplete();
+                                    } catch (Throwable t) {
+                                        em.onError(t);
+                                    }
+                                })
+                                .subscribeOn(new ComputationScheduler())
+                )
+                // 5) Обработка результатов в одном потоке
+                .observeOn(new SingleThreadScheduler())
+                // 6) Подписка и вывод
+                .subscribe(new Observer<String>() {
+                    @Override
+                    public void onNext(String item) {
+                        System.out.println(Thread.currentThread().getName() + " -> " + item);
+                    }
+
+                    @Override
+                    public void onError(Throwable t) {
+                        t.printStackTrace();
+                        doneLatch.countDown();
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        System.out.println("All done!");
+                        doneLatch.countDown();
+                    }
+                });
+
+        // Ждём завершения, иначе JVM может выйти раньше
+        doneLatch.await();
     }
 }
